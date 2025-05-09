2e63f1d324a1a29947fabf04ba8c22976101e7c4
YoshinoGento <k024g0013@g.neec.ac.jp>
2025-05-09 16:39:08 +0900

    レンダリングパイプライン　ヤヴァイ できた

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,108 +1,41 @@
+#include <Novice.h>
 #include "Matrix.h"
-//math.hではなくcmathをinclude
-#include <cmath>
+#include "math.h"
 
-//拡大縮小行列
-Matrix4x4 MatrixMath::MakeScaleMatrix(const Vector3& scale) {
 
-	Matrix4x4 result = { {
-	   {scale.x, 0,  0,  0},  // 横の大きさ（x方向）
-	   {0,  scale.y, 0,  0},  // 縦の大きさ（y方向）
-	   {0,  0,  scale.z, 0},  // 奥行きの大きさ（z方向）
-	   {0,  0,  0,  1}        // おまじない（そのままでOK）
-   } };
-
+Matrix4x4 MatrixMath::MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	float cot = (1.0f / tanf(fovY / 2.0f));
+	Matrix4x4 result = {};
+	result.m[0][0] = 1.0f / aspectRatio * cot;
+	result.m[1][1] = cot;
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
 	return result;
 }
 
-//X軸回転行列
-Matrix4x4 MatrixMath::MakeRotateXMatrix(float radian) {
-	Matrix4x4 result = { {
+Matrix4x4 MatrixMath::MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 result = {};
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);
+	result.m[2][2] = 1.0f / (farClip - nearClip);
+	result.m[0][3] = (left + right) / (left - right);
+	result.m[1][3] = (top + bottom) / (bottom - top);
+	result.m[2][3] = nearClip / (nearClip - farClip); 
+	result.m[3][3] = 1.0f;
 
-		{1.0f,0.0f,0.0f,0.0f},
-		{0.0f,std::cos(radian),std::sin(radian),0.0f},
-		{0.0f,-std::sin(radian),std::cos(radian),0.0f},
-		{0.0f,0.0f,0.0f,1.0f}
-	} };
-
-	return result;
+return result;
 }
 
-//Y軸回転行列
-Matrix4x4 MatrixMath::MakeRotateYMatrix(float radian) {
-	Matrix4x4 result = { {
-
-		{std::cos(radian),0.0f,-std::sin(radian),0.0f},
-		{0.0f,1.0f,0.0f,0.0f},
-		{std::sin(radian),0.0f,std::cos(radian),0.0f},
-		{0.0f,0.0f,0.0f,1.0f}
-	} };
+Matrix4x4 MatrixMath::MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result = {};
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + (width / 2.0f);
+	result.m[3][1] = top + (height / 2.0f);
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1.0f;
 	return result;
+
 }
-
-//Z軸回転行列
-Matrix4x4 MatrixMath::MakeRotateZMatrix(float radian) {
-	Matrix4x4 result = { {
-
-		{ std::cos(radian),std::sin(radian),0.0f,0.0f},
-		{-std::sin(radian),std::cos(radian),0.0f,0.0f},
-		{0.0f,0.0f,1.0f,0.0f},
-		{0.0f,0.0f,0.0f,1.0f}
-	} };
-	return result;
-}
-
-//平行移動行列
-Matrix4x4 MatrixMath::MakeTranslateMatrix(const Vector3& translate) {
-
-	Matrix4x4 result = { {
-		{1, 0, 0, 0},  // x方向にtranslateだけ動かす
-		{0, 1, 0, 0},  // y方向にtranslateだけ動かす
-		{0, 0, 1, 0},  // z方向にtranslateだけ動かす
-		{translate.x, translate.y, translate.z, 1}    // この行は変えない（特別な意味）
-	} };
-
-	return result;
-}
-
-
-Matrix4x4 MatrixMath::Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
-
-	Matrix4x4 result;
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			result.m[i][j] = 0;  // 初期化
-			for (int k = 0; k < 4; ++k) {
-				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
-			}
-		}
-	}
-
-	return result;
-}
-
-Matrix4x4 MatrixMath::MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
-	Matrix4x4 result;
-
-
-
-	//回転行列を生成する
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-
-	Matrix4x4 rotateMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
-
-	
-	result = {
-		scale.x * rotateMatrix.m[0][0],scale.x * rotateMatrix.m[0][1],scale.x * rotateMatrix.m[0][2],0.0f,
-		scale.y * rotateMatrix.m[1][0],scale.y * rotateMatrix.m[1][1],scale.y * rotateMatrix.m[1][2],0.0f,
-		scale.z * rotateMatrix.m[2][0],scale.z * rotateMatrix.m[2][1],scale.z * rotateMatrix.m[2][2],0.0f,
-		translate.x,translate.y,translate.z,1.0f
-	};
-
-	return result;
-}
-
-
-
--- Matrix.h
+++ Matrix.h
@@ -5,25 +5,14 @@ struct  Matrix4x4 {
 };
 namespace MatrixMath {
 
-	//1.拡大縮小行列
-	Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+	//1.透視投影行列
+	Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
 
-	//2.X軸回転行列
-	Matrix4x4 MakeRotateXMatrix(float radian);
-	//3.Y軸回転行列
-	Matrix4x4 MakeRotateYMatrix(float radian);
-	//4.Z軸回転行列
-	Matrix4x4 MakeRotateZMatrix(float radian);
-	//5.行列の積
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+	//2.正射影行列
+	Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
 
-	//平行移動行列
-	Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
-
-	//3次元アフィン変換行列
-	Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
-
-	
+	//3.ビューポート変換行列
+	Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
 	
 };
 
